855d5d6da3171e8943f1678339b8f07693f12f0d
sca4cs <shannon.atkinson.ca@gmail.com>
2019-03-28 12:33:10 -0400

    search by power

--- index.js
+++ index.js
@@ -22,10 +22,22 @@ server.get('/heroes', (req, res) => {
 
 server.get('/heroes/:name', (req, res) => {
     const name = req.params.name.toLowerCase();
-    db('heroes').where(db.raw('LOWER(REPLACE(REPLACE("name", " ", ""), "-", ""))'), name )
+    db('heroes').where(db.raw('LOWER(REPLACE(REPLACE("name", " ", ""), "-", ""))'), name)
     .then(heroes => {
         if (heroes.length === 0) {
         res.status(404).json({ message: "The requested hero does not exist in the database." });
+        } else 
+        res.status(200).json(heroes);
+    })
+    .catch(err => res.status(500).json(err));
+})
+
+server.get('/power/:power', (req, res) => {
+    const power = req.params.power.toLowerCase();
+    db('heroes').where('power', 'like', `%${power}%`)
+    .then(heroes => {
+        if (heroes.length === 0) {
+        res.status(404).json({ message: "The requested power does not exist in the database." });
         } else 
         res.status(200).json(heroes);
     })
